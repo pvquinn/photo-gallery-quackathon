f524e3b62032879079382c32c07c935701c3aefe
Pillar Quinn <pillaraloro@gmail.com>
2023-03-03 22:02:19 -0800

    create basic local storage functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function addImage1() {
+    localStorage.setItem('image1', 'big-duck.jpg');
+}
+
+function populate() {
+    const image = localStorage.getItem('image1');
+    document.getElementById("collectedImage1").src="image";
+}
+
+function remove() {
+    localStorage.removeItem('image1');
+    const element = document.getElementById("collectedImage1");
+    element.remove();
+}
